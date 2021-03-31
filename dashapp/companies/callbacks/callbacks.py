86dfad8511d3cddceb3af6e6a84730b0006df02c
masete <nicholasmasete72@gmail.com>
2021-03-31 09:43:10 +0300

    Refactored companies

---
+++ dashapp/companies/callbacks/callbacks.py
@@ -0,0 +1,119 @@
+from dash.dependencies import Input, Output, State
+import dash_html_components as html
+import dash_core_components as dcc
+import dash_table
+from flask_login import current_user
+import pandas as pd
+from collections import OrderedDict
+from sqlalchemy.exc import IntegrityError
+# import dashapp.transactions.layout
+
+
+def register_callbacks(app):
+    from app import db
+    from app.models import Entreprenuers
+
+    with app.server.app_context():
+        entre = db.session.query(Entreprenuers)
+        data = pd.read_sql(entre.statement, entre.session.bind)
+
+        dc = [i.lower() for i in list(data.columns)]
+        data.columns = dc
+
+    operators = [['ge ', '>='],
+                 ['le ', '<='],
+                 ['lt ', '<'],
+                 ['gt ', '>'],
+                 ['ne ', '!='],
+                 ['eq ', '='],
+                 ['contains '],
+                 ['datestartswith ']]
+
+    def split_filter_part(filter_part):
+        for operator_type in operators:
+            for operator in operator_type:
+                if operator in filter_part:
+                    name_part, value_part = filter_part.split(operator, 1)
+                    name = name_part[name_part.find('{') + 1: name_part.rfind('}')]
+
+                    value_part = value_part.strip()
+                    v0 = value_part[0]
+                    if (v0 == value_part[-1] and v0 in ("'", '"', '`')):
+                        value = value_part[1: -1].replace('\\' + v0, v0)
+                    else:
+                        try:
+                            value = float(value_part)
+                        except ValueError:
+                            value = value_part
+
+                    # word operators need spaces after them in the filter string,
+                    # but we don't want these later
+                    return name, operator_type[0].strip(), value
+
+        return [None] * 3
+
+    @app.callback(
+        Output('table-paging-with-graph', "data"),
+        Input('table-paging-with-graph', "page_current"),
+        Input('table-paging-with-graph', "page_size"),
+        Input('table-paging-with-graph', "sort_by"),
+        Input('table-paging-with-graph', "filter_query"))
+    def update_table(page_current, page_size, sort_by, filter):
+        filtering_expressions = filter.split(' && ')
+        dff = data
+        for filter_part in filtering_expressions:
+            col_name, operator, filter_value = split_filter_part(filter_part)
+
+            if operator in ('eq', 'ne', 'lt', 'le', 'gt', 'ge'):
+                # these operators match pandas series operator method names
+                dff = dff.loc[getattr(dff[col_name], operator)(filter_value)]
+            elif operator == 'contains':
+                dff = dff.loc[dff[col_name].str.contains(filter_value)]
+            elif operator == 'datestartswith':
+                # this is a simplification of the front-end filtering logic,
+                # only works with complete fields in standard format
+                dff = dff.loc[dff[col_name].str.startswith(filter_value)]
+
+        if len(sort_by):
+            dff = dff.sort_values(
+                [col['id'] for col in sort_by],
+                ascending=[
+                    col['direction'] == 'asc'
+                    for col in sort_by
+                ],
+                inplace=False
+            )
+
+        return dff.iloc[
+               page_current * page_size: (page_current + 1) * page_size
+               ].to_dict('records')
+
+    @app.callback(
+            Output('table-paging-with-graph-container', "children"),
+            Input('table-paging-with-graph', "data"))
+    def update_graph(rows):
+        dff = pd.DataFrame(rows)
+        return html.Div(
+            [
+                dcc.Graph(
+                    id=column,
+                    figure={
+                        "data": [
+                            {
+                                "x": dff["company_name"],
+                                "y": dff[column] if column in dff else [],
+                                "type": "bar",
+                                "marker": {"color": "#0074D9"},
+                            }
+                        ],
+                        "layout": {
+                            "xaxis": {"automargin": True},
+                            "yaxis": {"automargin": True},
+                            "height": 250,
+                            "margin": {"t": 10, "l": 10, "r": 10},
+                        },
+                    },
+                )
+                for column in ["last_funding_round_raised_amount", "company_age", "number_of_employees"]
+            ]
+        )
